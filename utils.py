dacc2d9d6d735597e9e46e0c75d125e54695b4f5
Ondrej Lukas <ondrej.lukas95@gmail.com>
2023-07-17 15:56:22 +0200

    preprocessing function

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ import pandas as pd
 def load_data_from_csv(filename:str):
     df = pd.read_csv(filename)
     return df
+
+def prepare_data_classification(filename:str)->tuple:
+    data = load_data_from_csv("combined_all_days_and_clients.csv")
+    x = data.copy()
+    x = x.drop(columns=["label", "detailedlabel", "day", "hour", "id.orig_h", "id.resp_h", "id.resp_p", "proto"])
+    y = data["label"]
+    return x,y
